9ddb6f8eee1bdb4816115365e482906ef5b57c09
Amin Javed <ajaved@student.tgm.ac.at>
2017-03-27 10:29:52 +0200

    commit

---
+++ src/amin/Test1703.java
@@ -0,0 +1,14 @@
+package amin;
+
+public class Test1703 {
+
+	public Test1703() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
